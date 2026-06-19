7b9505d7645ceb1d348bba678d3f62f1bd107d99
Zaheer Hussain <zah82560@gmail.com>
2026-06-19 18:18:55 +0530

    'finalCommit'

--- src/services/creditScoreService.js
+++
@@ -1,6 +0,0 @@
-import API from "./api";
-
-// ✅ Get Credit Score by userId
-export const getCreditScore = (userId) => {
-  return API.get(`/creditscore/${userId}`);
-};
--- src/services/portfolioService.js
+++
@@ -1,7 +0,0 @@
-
-import API from "./api";
-
-// ✅ Get portfolio by accountId
-export const getPortfolio = (accountId) => {
-  return API.get(`/portfolio/${accountId}`);
-};
